9e15ac016caf1255db87f2de9918421a27dd3824
clstlaurent <clstlaurent@folivoraapps.com>
2019-02-01 22:57:42 -0500

    Localize report structure and dedicated folder space. Rosparam update

--- command_unit/scripts/Master.py
+++ command_unit/scripts/Master.py
@@ -10,46 +10,70 @@
 import rospy
 from localize_service.srv import localize_service
 
+def initialize():
+    rospy.init_node('command_unit', anonymous=False)
+
+def run_IPAS():
+    pass
 
 def run_Localize():
     while(rospy.get_param('/lightswitch')=='on'):
 
-        #If we set sample to true, begin a new localization test
-        if(rospy.get_param('/sample')==True):
-            rospy.set_param('/samle', False) #Reset the parameter to avoid loop
+        while(rospy.get_param('/localize_test/reconfigure')==False): #Set true when done sampling
 
-            sampleCount = 100
-            rospy.wait_for_service('localize_service')
-            localizeService = rospy.ServiceProxy('localize_service', localize_service)
-            localizeData = localizeService(sampleCount) #Call service from UAV, stored as tuple of arrays
+            #If we set sample to true, begin a new localization test
+            if(rospy.get_param('/localize_test/sample')==True):
 
-            #localizeData = {localizeData.posx, localizeData.posy, localizeData.posz}
+                rospy.set_param('/localize_test/sample', False) #Reset the parameter to avoid loop
 
-            #Then call function from localization report and pass in data
-            #Data: LocalizeData, AnchorPositions, TestLocation
+                sampleCount = 100 #Default Sample count for service
+                rospy.wait_for_service('localize_service') #Wait for service to be ready
+                localizeService = rospy.ServiceProxy('localize_service', localize_service)
+                localizeData = localizeService(sampleCount) #Call service from UAV, stored as tuple of arrays
+                #localizeData Returns: {localizeData.posx, localizeData.posy, localizeData.posz}
 
-        else:
-            time.sleep(1) #Sleep while waiting
+                #Then call function from localization report and pass in data
+                #Data: LocalizeData, AnchorPositions, TestLocation
+                #Goal: Generate CSV file of sample data, sample location,
+                #   sample mean, sample variance, sample std, sample error
 
+            else:
+                time.sleep(1) #Sleep while waiting
 
-def initialize():
-    rospy.init_node('command_unit', anonymous=False)
+        #Reset parameters for next anchor configuration and new samples
+        rospy.set_param('/localize_test/reconfigure', False)
+        rospy.set_param('localize_test/sample', False)
+
+        #Call finalize report, which then takes all the accumulated data and
+        #does Matplotlib to graphically output the results for configuration samples
+        #Input: None
+        #Goal: Use CSV data created in configuration to create graphic representation
+
+def run_FlightTest():
+    pass
+
+def run_GroundTest():
+    pass
+
+def run_IPAS_sim():
+    pass
+
 
 def run():
     #System Mode ('IPAS', 'Localize', 'Flight_Test', 'Ground_Test')
     mode = rospy.get_param('/mode')
 
     if(mode=='IPAS'):
-        #Run IPAS Algorithm
+        run_IPAS()
         pass
     elif(mode=='Localize'):
         run_Localize()
     elif(mode=='Flight_Test'):
-        #Run Flight Test
-        pass
+        run_FlightTest()
     elif(mode=='Ground_Test'):
-        #Run Ground Test
-        pass
+        run_GroundTest()
+    elif(mode=='IPAS_Sim'):
+        run_IPAS_sim()
     else:
         print('Failed to initialize')
 
---
+++ command_unit/scripts/localization/localization_csv_handler.py
@@ -0,0 +1,12 @@
+#Populate CSV Files in structured format with localization data
+
+############################   LIBRARIES   ############################
+
+#CSV library
+import csv as csv
+
+#Math Tools
+from itertools import product, combinations
+import numpy as np
+import math as math
+import pandas as pd

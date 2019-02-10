e1730e089739ad10a63ac49af15b847f228a8739
clstlaurent <clstlaurent@wpi.edu>
2019-02-10 15:38:04 -0500

    Reformated and compartamentalized IPozyx

--- ifutr/scripts/localization/IPozyx.py
+++ ifutr/scripts/localization/IPozyx.py
@@ -1,74 +1,67 @@
-#Pozyx Interface
 
-from pypozyx import *
-from pypozyx.definitions.constants import *
-from pypozyx.definitions.registers import *
+from time import sleep
 
+from pypozyx import (POZYX_POS_ALG_UWB_ONLY, POZYX_3D, Coordinates, POZYX_SUCCESS, PozyxConstants, version,
+                     DeviceCoordinates, PozyxSerial, get_first_pozyx_serial_port, SingleRegister, DeviceList, PozyxRegisters)
+from pythonosc.udp_client import SimpleUDPClient
 
-class IPozyx:
-    def __init__(self, anchors, dimension=POZYX_3D, algorithm=POZYX_POS_ALG_UWB_ONLY):
+from pypozyx.tools.version_check import perform_latest_version_check
+
+class IPozyx(object):
+    """Continuously calls the Pozyx positioning function and prints its position."""
+
+    def __init__(self, anchors):
 
         self.serial_port = get_first_pozyx_serial_port()
-	if self.serial_port is None:
-		print('No Pozyx Connected')
-	self.remote_id = 0x683e
-	self.remote = False
-	if not self.remote:
-		self.remote_id = None
+        if self.serial_port is None:
+            print("No Pozyx connected. Check your USB cable or your driver!")
 
         self.pozyx = PozyxSerial(self.serial_port)
 
-        print(self.pozyx)
+        if(anchors.get('count')==4):
 
-	self.use_processing = False
+            self.anchors = [DeviceCoordinates(0x6110, 1,
+                                              Coordinates(anchors.get('0x6110')[0],
+                                                          anchors.get('0x6110')[1],
+                                                          anchors.get('0x6110')[2])),
+                            DeviceCoordinates(0x6115, 1,
+                                              Coordinates(anchors.get('0x6115')[0],
+                                                          anchors.get('0x6115')[1],
+                                                          anchors.get('0x6115')[2])),
+                            DeviceCoordinates(0x6117, 1,
+                                              Coordinates(anchors.get('0x6117')[0],
+                                                          anchors.get('0x6117')[1],
+                                                          anchors.get('0x6117')[2])),
+                            DeviceCoordinates(0x611e, 1,
+                                              Coordinates(anchors.get('0x611e')[0],
+                                                          anchors.get('0x611e')[1],
+                                                          anchors.get('0x611e')[2]))]
 
-        self.ip = '127.0.0.1'
-        self.network_port = 8888
 
-	print(anchors.get('0x6110')[0])
+        self.algorithm = PozyxConstants.POSITIONING_ALGORITHM_UWB_ONLY
+        self.dimension = PozyxConstants.DIMENSION_3D
+        self.height = 1000
 
-        self.anchors = [DeviceCoordinates(0x6110, 1,
-                                          Coordinates(anchors.get('0x6110')[0],
-                                                      anchors.get('0x6110')[1],
-                                                      anchors.get('0x6110')[2])),
-                        DeviceCoordinates(0x6115, 1,
-                                          Coordinates(anchors.get('0x6115')[0],
-                                                      anchors.get('0x6115')[1],
-                                                      anchors.get('0x6115')[2])),
-                        DeviceCoordinates(0x6117, 1,
-                                          Coordinates(anchors.get('0x6117')[0],
-                                                      anchors.get('0x6117')[1],
-                                                      anchors.get('0x6117')[2])),
-                        DeviceCoordinates(0x611e, 1,
-                                          Coordinates(anchors.get('0x611e')[0],
-                                                      anchors.get('0x611e')[1],
-                                                      anchors.get('0x611e')[2]))]
-
-        self.algorithm = algorithm
-        self.dimension = dimension
-	self.height = 1000
-
-        self.setup()
     def setup(self):
         self.setAnchorsManual()
+        self.printPublishConfigurationResult()
+
+    def run(self):
+        """Performs positioning and displays/exports the results."""
+        position = Coordinates()
+        status = self.pozyx.doPositioning(
+            position, self.dimension, self.height, self.algorithm, remote_id=self.remote_id)
+        if status == POZYX_SUCCESS:
+            return position
+        else:
+            pass
 
     def setAnchorsManual(self):
-            """Adds the manually measured anchors to the Pozyx's device list one for one."""
-            status = self.pozyx.clearDevices()
-            for a in self.anchors:
-                status &= self.pozyx.addDevice(a)
-            if len(self.anchors) > 4:
-                status &= self.pozyx.setSelectionOfAnchors(ANCHOR_SELECT_AUTO, len(self.anchors))
-	    print(status)
-            return status
-
-    def getPosition(self):
-        pos = Coordinates()
-        if self.pozyx.doPositioning(pos, self.dimension, self.height, self.algorithm) != POZYX_SUCCESS:
-            #self.print_error_code("positioning")
-            print('Error during Positioning')
-	#error = PositionError()
-        #if self.pozyx.getPositionError(error) != POZYX_SUCCESS:
-        #    log("Failed to get positioning error.")
-        print(pos)
-        return pos
+        """Adds the manually measured anchors to the Pozyx's device list one for one."""
+        status = self.pozyx.clearDevices(remote_id=self.remote_id)
+        for anchor in self.anchors:
+            status &= self.pozyx.addDevice(anchor, remote_id=self.remote_id)
+        if len(self.anchors) > 4:
+            status &= self.pozyx.setSelectionOfAnchors(PozyxConstants.ANCHOR_SELECT_AUTO, len(self.anchors),
+                                                       remote_id=self.remote_id)
+        return status
--- ifutr/scripts/uav.py
+++ ifutr/scripts/uav.py
@@ -4,7 +4,6 @@ import rospy
 import pypozyx
 
 from localization.IPozyx import IPozyx
-from localization.IPozyx2 import IPozyx2
 from time import sleep
 
 from pypozyx import (POZYX_POS_ALG_UWB_ONLY, POZYX_3D, Coordinates, POZYX_SUCCESS, PozyxConstants, version,
@@ -24,68 +23,13 @@ def run_IPAS():
 
 def localize_serviceHandler(request):
     print('Starting the handler')
-    #Initialize the pozyx interface
-    check_pypozyx_version = True
-    if check_pypozyx_version:
-        perform_latest_version_check()
 
-    # shortcut to not have to find out the port yourself
-    serial_port = get_first_pozyx_serial_port()
-    print(serial_port)
-    if serial_port is None:
-        print("No Pozyx connected. Check your USB cable or your driver!")
-
-
-    remote_id = 0x683e                 # remote device network ID
-    remote = False                   # whether to use a remote device
-    if not remote:
-        remote_id = None
-
-    # enable to send position data through OSC
-    use_processing = False
-
-    # configure if you want to route OSC to outside your localhost. Networking knowledge is required.
-    ip = "127.0.0.1"
-    network_port = 8888
-
-    osc_udp_client = None
-    if use_processing:
-        osc_udp_client = SimpleUDPClient(ip, network_port)
-
-    # necessary data for calibration, change the IDs and coordinates yourself according to your measurement
+    # necessary data for calibration,
+    #change the IDs and coordinates yourself according to your measurement
     anchors = rospy.get_param('/anchorpose')
-    anchors = [DeviceCoordinates(0x6110, 1,
-                                      Coordinates(anchors.get('0x6110')[0],
-                                                  anchors.get('0x6110')[1],
-                                                  anchors.get('0x6110')[2])),
-                    DeviceCoordinates(0x6115, 1,
-                                      Coordinates(anchors.get('0x6115')[0],
-                                                  anchors.get('0x6115')[1],
-                                                  anchors.get('0x6115')[2])),
-                    DeviceCoordinates(0x6117, 1,
-                                      Coordinates(anchors.get('0x6117')[0],
-                                                  anchors.get('0x6117')[1],
-                                                  anchors.get('0x6117')[2])),
-                    DeviceCoordinates(0x611e, 1,
-                                      Coordinates(anchors.get('0x611e')[0],
-                                                  anchors.get('0x611e')[1],
-                                                  anchors.get('0x611e')[2]))]
 
-    # positioning algorithm to use, other is PozyxConstants.POSITIONING_ALGORITHM_TRACKING
-    algorithm = PozyxConstants.POSITIONING_ALGORITHM_UWB_ONLY
-    # positioning dimension. Others are PozyxConstants.DIMENSION_2D, PozyxConstants.DIMENSION_2_5D
-    dimension = PozyxConstants.DIMENSION_3D
-    # height of device, required in 2.5D positioning
-    height = 1000
-
-    pozyx = PozyxSerial(serial_port)
-    r = IPozyx2(pozyx, osc_udp_client, anchors, algorithm, dimension, height, remote_id)
+    r = IPozyx(anchors)
     r.setup()
-    #while True:
-    #    r.loop()
-
-
-    #pozyx = IPozyx(anchors)
 
     x_buff = []
     y_buff = []

ed6dd55cceb8153938df4fe9876eb6a23c2d23ab
Chase1325 <clstlaurent@wpi.edu>
2019-03-20 14:11:49 -0400

    get rid of udp

--- ifutr/scripts/sensors/Pozyx.py
+++ ifutr/scripts/sensors/Pozyx.py
@@ -2,7 +2,7 @@
 from time import sleep
 
 from pypozyx import (POZYX_POS_ALG_UWB_ONLY, POZYX_3D, Coordinates, POZYX_SUCCESS, PozyxConstants, version, DeviceCoordinates, PozyxSerial, get_first_pozyx_serial_port, SingleRegister, DeviceList, PozyxRegisters)
-from pythonosc.udp_client import SimpleUDPClient
+#from pythonosc.udp_client import SimpleUDPClient
 
 from pypozyx.tools.version_check import perform_latest_version_check
 

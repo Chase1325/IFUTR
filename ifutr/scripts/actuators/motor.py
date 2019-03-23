ea633eb72cf4c3d58b2d09a8495b24b452710b36
Chase1325 <clstlaurent@wpi.edu>
2019-03-23 12:37:42 -0400

    move in a square?

--- ifutr/scripts/actuators/motor.py
+++ ifutr/scripts/actuators/motor.py
@@ -3,20 +3,39 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-pwm_pin_l=37
-sig1_pin_l=40
-sig2_pin_l=38
+pwm_pin_r=37
+sig1_pin_r=40
+sig2_pin_r=38
+
+pwm_pin_l=11
+sig1_pin_l=13
+sig2_pin_l=15
 
 GPIO.setup(pwm_pin_l, GPIO.OUT)
-pwm = GPIO.PWM(pwm_pin_l, 10000)
-pwm.start(0)
+pwm_l = GPIO.PWM(pwm_pin_l, 10000)
+pwm_l.start(0)
 GPIO.setup(sig1_pin_l, GPIO.OUT)
 GPIO.setup(sig2_pin_l, GPIO.OUT)
 
+GPIO.setup(pwm_pin_r, GPIO.OUT)
+pwm_r = GPIO.PWM(pwm_pin_r, 10000)
+pwm_r.start(0)
+GPIO.setup(sig1_pin_r, GPIO.OUT)
+GPIO.setup(sig2_pin_r, GPIO.OUT)
+
 while 1:
 
-    #i=0
-    #while i<15:
+    i=0
+    while(i<15):
+        GPIO.output(sig1_pin_r, GPIO.HIGH)
+        GPIO.output(sig2_pin_r, GPIO.LOW)
+        pwm.ChangeDutyCycle(100)
+        i+=1
+
+    while(i<15):
         GPIO.output(sig1_pin_l, GPIO.HIGH)
         GPIO.output(sig2_pin_l, GPIO.LOW)
+        GPIO.output(sig1_pin_r, GPIO.HIGH)
+        GPIO.output(sig2_pin_r, GPIO.LOW)
         pwm.ChangeDutyCycle(100)
+        i+=1

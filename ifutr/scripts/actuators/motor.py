b96548de2d92c910a7e654e7c96640b6c7508a02
Chase1325 <clstlaurent@wpi.edu>
2019-03-23 14:19:58 -0400

    demo material

--- ifutr/scripts/actuators/motor.py
+++ ifutr/scripts/actuators/motor.py
@@ -27,19 +27,59 @@ GPIO.setup(sig2_pin_r, GPIO.OUT)
 while 1:
     pwm_l.ChangeDutyCycle(100)
     pwm_r.ChangeDutyCycle(100)
+
+
+
     i=0
-    while(i<50):
+    while(i<100):
+        GPIO.output(sig1_pin_l, GPIO.HIGH)
+        GPIO.output(sig2_pin_l, GPIO.LOW)
         GPIO.output(sig1_pin_r, GPIO.HIGH)
         GPIO.output(sig2_pin_r, GPIO.LOW)
-        GPIO.output(sig1_pin_l, GPIO.LOW)
-        GPIO.output(sig2_pin_l, GPIO.LOW)
 
         i+=1
+        time.sleep(.1)
+        print('go straight')
+
+    time.sleep(3)
+
+    j=0
+    while(j<25):
+        GPIO.output(sig1_pin_l, GPIO.HIGH)
+        GPIO.output(sig2_pin_l, GPIO.LOW)
+        GPIO.output(sig1_pin_r, GPIO.LOW)
+        GPIO.output(sig2_pin_r, GPIO.LOW)
+
+        j+=1
         time.sleep(0.1)
         print('turn')
 
     i=0
-    while(i<10):
+    while(i<50):
+        GPIO.output(sig1_pin_l, GPIO.HIGH)
+        GPIO.output(sig2_pin_l, GPIO.LOW)
+        GPIO.output(sig1_pin_r, GPIO.HIGH)
+        GPIO.output(sig2_pin_r, GPIO.LOW)
+
+        i+=1
+        time.sleep(.1)
+        print('go straight')
+
+    time.sleep(3)
+
+    j=0
+    while(j<35):
+        GPIO.output(sig1_pin_l, GPIO.HIGH)
+        GPIO.output(sig2_pin_l, GPIO.LOW)
+        GPIO.output(sig1_pin_r, GPIO.LOW)
+        GPIO.output(sig2_pin_r, GPIO.LOW)
+
+        j+=1
+        time.sleep(0.1)
+        print('turn')
+
+    i=0
+    while(i<100):
         GPIO.output(sig1_pin_l, GPIO.HIGH)
         GPIO.output(sig2_pin_l, GPIO.LOW)
         GPIO.output(sig1_pin_r, GPIO.HIGH)
--- ifutr/scripts/ugv.py
+++ ifutr/scripts/ugv.py
@@ -1,1 +1,63 @@
 ### Main file for UGV
+
+
+
+
+
+
+
+
+def run_GroundTest():
+    while(rospy.get_param('/lightswitch'==True)):
+
+        #2) RECONFIGURE
+        if(rospy.get_param('/ugv/mode'=='reconfig')):
+
+            #Grab List of waypoints and control the vehicle to them
+            x_waypoints = rospy.get_param('/ugv/waypoints')
+            y_waypoints = rospy.get_param('/ugv/waypoints')
+
+
+            print(waypoints)
+
+            for target in waypoints:
+
+                #input the target into the PID controller
+                #grab the current location from the localization software
+
+
+            pass
+        else:
+            pass
+
+
+
+
+
+
+
+
+def run():
+    #System Mode ('IPAS', 'Localize', 'Flight_Test', 'Ground_Test')
+    #Selected mode determined from rosparam values
+    mode = rospy.get_param('/mode')
+
+    if(mode=='IPAS'):
+        run_IPAS()
+    elif(mode=='Localize'):
+        run_Localize()
+    elif(mode=='Flight_Test'):
+        pass#run_FlightTest()
+    elif(mode=='Ground_Test'):
+        run_GroundTest()
+    elif(mode=='IPAS_Sim'):
+        pass #Drone not required
+    else:
+        print('Failed to initialize')
+
+
+
+if __name__ == "__main__":
+
+    initialize()
+    run()

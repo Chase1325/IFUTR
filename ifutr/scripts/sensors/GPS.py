affdefd88c0bbf54abf2e51fd4efe9f55022189a
Chase1325 <clstlaurent@wpi.edu>
2019-03-24 12:32:12 -0400

    new gps config

---
+++ ifutr/scripts/sensors/GPS.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from time import sleep
+
+import rospy
+from std_msgs.msg import Int16
+from geometry_msgs.msg import PoseStamped
+from ifutr.msg import Pozyx_Pose
+
+
+
+
+
+class Fake_GPS(object):
+
+    def __init__(self, anchors):
+        self.r = rospy.Rate(5)
+        self.pub = rospy.Publisher('/mavros/fake_gps/mocap/pose', PoseStamped, queue_size=10)
+        self.sub = rospy.Subscriber('/pose', PoseStamped, poseCallback)
+        self.gps = PoseStamped()
+
+
+    def poseCallback(self, msg):
+        self.gps = msg
+        print(self.gps)
+
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.pub.publish(self.gps)
+            self.r.sleep()
+
+
+def initialize():
+    rospy.init_node('Fake_GPS', anonymous=False)
+
+
+
+if __name__ == "__main__":
+    initialize()
+    run()
